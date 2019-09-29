a5bb4755e17f3774e0c5fe05936f8f54efa57516
Kiel Lewis <kiel.lewis@mailchimp.com>
2019-09-29 12:32:55 -0400

    built component foundations and rendered in App.js, also connected to chatkit api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { ChatManager, TokenProvider } from "@pusher/chatkit-client/react-native";
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { tokenUrl, instanceLocator } from './config'
+
+class App extends React.Component {
+  
+  componentDidMount() {
+    const chatManager = new ChatManager({
+      instanceLocator,
+      userId: 'klew5',
+      tokenProvider: new TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: "56b26459-b5e3-4ac2-9070-89619c2a27f7",
+        hooks: {
+          onMessage: message => {
+            console.log("message.text", message.text);
+          }
+        }
+      });
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <RoomList />
+        <MessageList />
+        <SendMessageForm /> 
+        <NewRoomForm />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const tokenUrl =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/f9d07966-7e0e-422d-87d3-fb02649446cb/token"; 
+
+const instanceLocator = "v1:us1:f9d07966-7e0e-422d-87d3-fb02649446cb";
+
+export { tokenUrl, instanceLocator }
